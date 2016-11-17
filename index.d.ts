8390a42c1639d0ca3630548c399b22d0e7a41f62
Alejandro Perdomo <aperdomobo@gmail.com>
2016-11-17 15:05:07 -0500

    first version

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for sync-request
+// Project: https://github.com/ForbesLindesay/sync-request
+// Definitions by: Alejandro Perdomo <https://github.com/aperdomob/>
+
+
+declare function request(method:string, url:string, options:any) : any;
+
+export = request;
